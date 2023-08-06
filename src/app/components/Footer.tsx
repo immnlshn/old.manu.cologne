0fc12c2014520a3f3cc09be7f100e47bfb458d42
Immanuel Sohn <info@immanuelsohn.de>
2023-08-06 02:32:08 +0200

    updated footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,8 @@ export default function Footer(){
     return(
         <footer className="absolute bottom-0 flex flex-col items-center justify-center gap-2 p-5 w-screen">
             <p className="text-center">© 2023 manu.cologne</p>
-            <p className="text-center">Made with <span><FontAwesomeIcon className="text-blue-500" icon={faHeart} size="xl"></FontAwesomeIcon></span> and Next.JS</p>
+            <p className="text-center">Made with <span><FontAwesomeIcon className="text-blue-500" icon={faHeart} size="xl"></FontAwesomeIcon></span>, TailwindCSS and Next.JS</p>
+            <p>View source-code <a className="text-blue-400" href="https://github.com/immnlshn/manu.cologne" target="_blank">here</a>!</p>
         </footer>
     )
 }
